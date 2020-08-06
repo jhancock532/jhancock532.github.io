a32131e13b612e0311153319d63a49eb037c0c37
James Hancock <jhancock532@gmail.com>
2020-08-06 13:17:59 +0100

    Menu overlay and mobile styling

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,11 @@
-let menuOpen = false, menuItemsContainer, menuItems;
+let menuOpen = false;
+let menuItemsContainer, menuItems, menuToggleButton, menuBackgroundOverlay;
 
 window.onload = function() {
   menuItemsContainer = document.getElementById("menu-options-container");
   menuItems = menuItemsContainer.getElementsByClassName("menu__option");
+  menuToggleButton = document.getElementById("menu-toggle-button");
+  menuBackgroundOverlay = document.getElementById("menu-background-overlay");
 
   for (let i = 0; i < menuItems.length; ++i) {
     let transitionTime = ((0.4 - i / 20) * i).toString();
@@ -10,13 +13,10 @@ window.onload = function() {
     
     menuItems[i].style.transition = transitionText;
   }
-
-  console.log("Window loaded.")
 };
 
 
 function toggleMenu(){
-  console.log("Function called.");
   if (menuOpen){
     for (let i = 0; i < menuItems.length; ++i) {
       menuItems[i].style.top = "0px";
@@ -28,6 +28,14 @@ function toggleMenu(){
       
       menuItems[i].style.transition = transitionText;
     }
+
+    menuToggleButton.innerHTML = '<i class="fontello-icon menu__burger-icon">&#xe804;</i>MENU';
+    menuBackgroundOverlay.style.opacity = "0%";
+    menuBackgroundOverlay.style.zIndex = "-2";
+
+    menuBackgroundOverlay.style.transition = "opacity 1s ease-in, z-index 0.001s linear 1s";
+    
+
   } else {
     for (let i = 0; i < menuItems.length; ++i) {
       menuItems[i].style.top = (i * 60).toString() + "px";
@@ -37,7 +45,15 @@ function toggleMenu(){
       let transitionText = "top " + transitionTime + "s cubic-bezier(0.34, 1.56, 0.64, 1), opacity " + transitionTime + "s cubic-bezier(0.34, 1.56, 0.64, 1)";
       
       menuItems[i].style.transition = transitionText;
+
+      menuToggleButton.innerHTML = '<i class="fontello-icon menu__close-icon">&#xe805;</i>';
     }
+
+    menuBackgroundOverlay.style.opacity = "80%";
+    menuBackgroundOverlay.style.zIndex = "2";
+
+    
+    menuBackgroundOverlay.style.transition = "opacity 0.8s ease-out";
   }
 
   menuOpen = !menuOpen;
